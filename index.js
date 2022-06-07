f61aabb8dc1c1cd15add4a860676fa483307b30f
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-06-07 15:20:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,25 @@ class SinglyLinkedList{
       }
       return false
   }
+    
+    
+      insert(index, val){
+if(index < 0 || index > this.length) return null 
+if(index === this.length) return this.push(val)
+if(index === 0) return this.unshift(val)
+
+
+let newNode = new Node(val) 
+let prev = this.get(index - 1)
+let temp = prev.next 
+prev.next = newNode
+newNode.next = temp 
+this.length++ 
+return true
+    }
+
 }
+
 
 
 var list = new SinglyLinkedList()
