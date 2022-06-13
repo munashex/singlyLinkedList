867f6e44940a06cfb994368f18a470a76a2c8e8e
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-06-13 09:00:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ class SinglyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+    
+    
+    /*
+    With pushing, we need to think about popping, deleting the last element. If there is no node, return undefined,
+    else, loop through the list until we reach the tail, set the next property of the second last node to be null,
+    make the second last to be the tail, don’t forget to decrement the size of the list.
+    */
     push(val){
         var newNode = new Node(val);
         if(!this.head){
@@ -24,6 +31,12 @@ class SinglyLinkedList{
         return this;
     }
     
+    /*
+    With pushing, we need to think about popping, deleting the last element.
+    If there is no node, return undefined, else, loop through the list until we reach the tail, 
+    set the next property of the second last node to be null, make the second last to be the tail, 
+    don’t forget to decrement the size of the list.
+    */
     pop(){
         if(!this.head) return undefined  
 
@@ -40,7 +53,12 @@ class SinglyLinkedList{
         return current
     }
 
-
+    
+    /*
+    For deleting the first element, shifting, as usual, check if the list is empty.
+    First, store the current head in a variable, set the head to be the current head’s next,
+    decrement the length.
+    */
     shift() {
         if(!this.head) return undefined
         let currentHead = this.head
@@ -51,7 +69,13 @@ class SinglyLinkedList{
         }
         return currentHead
     }
-
+    
+    
+    /*
+    To insert a node at the beginning of the list, check if the list is empty, 
+    if not, we set the current head to be the next attribute of the incoming node, 
+    increment the size.
+    */
     unshift(val){
         let newNode = new Node(val)
         if(!this.head) {
@@ -65,6 +89,11 @@ class SinglyLinkedList{
         return this
     }
     
+    /*
+    Even though linked list doesn’t have indexes, we are still able to find the node by given index.
+    First make sure the given index is greater than zero and smaller or equal to the length of the list. 
+    Than we loop through the list until we reach the index.
+    */
     get(index) {
         if(index < 0 || index >= this.length) null 
 
@@ -76,6 +105,10 @@ class SinglyLinkedList{
         }
         return current
     }
+    
+    /*
+    What if we want to change a node in our list? We find the node with get(), and set the node with given data.
+    */
 
   set(index, val) {
       let foundNode = this.get(index) 
@@ -87,6 +120,12 @@ class SinglyLinkedList{
       return false
   }
     
+   /*
+   When we want to insert a new node into the list, first check if the index is greater than 0 and smaller than the length.
+   If index is the length, we just use push(), if the index is 0, we use unshift().
+   For other indexes, we need to get the node at the index-1, and set the next property of that node to be the new node, 
+   and the next property of the new node to be the previous next property, then we increment the length.
+ */
     
       insert(index, val){
 if(index < 0 || index > this.length) return null 
@@ -102,6 +141,13 @@ newNode.next = temp
 this.length++ 
 return true
     }
+    
+    /*
+    Unlike pop and unshift, remove function will remove the node by given index. As usual, check if the index is valid,
+    if index equals to length-1 or 0, use pop or shift.
+    Otherwise, we get the node at the index-1, set the next property on
+    that node to be the next of the next property, after, we decrement the size.
+    */
 
     remove(index) {
         if(index  < 0 || index >= this.length) return null
@@ -117,6 +163,11 @@ return true
     }
     
     
+    /*  
+    The ultimate reverse question! 
+    How do we reverse the list? First, we swap head and tail, declare next and previous, 
+    set the previous as null. We loop through the list.
+    */
     reverse(){
       var node = this.head;
       this.head = this.tail;
