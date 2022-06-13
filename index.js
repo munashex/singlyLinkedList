b08e1e692325346a1130815468426900305a3596
munashe madhuna <79254789+munashex@users.noreply.github.com>
2022-06-13 08:25:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,22 @@ return true
        return removed
         
     }
+    
+    
+    reverse(){
+      var node = this.head;
+      this.head = this.tail;
+      this.tail = node;
+      var next;
+      var prev = null;
+      for(var i = 0; i < this.length; i++){
+        next = node.next;
+        node.next = prev;
+        prev = node;
+        node = next;
+      }
+      return this;
+    }
 }
 
 
